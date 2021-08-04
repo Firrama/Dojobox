9ada5f305fc97482ae0e2ab73e93f06304195020
Ilham <azisilham08@gmail.com>
2021-08-04 19:11:50 +0700

    Update javascript1.js

--- BelajarJs/javascript1.js
+++ BelajarJs/javascript1.js
@@ -1,1 +1,15 @@
+/* alert("selamat datang dikelas ini...");
+var nama =prompt("masukkan nama kamu...");
+document.write("selamat data ka "+ nama + '<br>');
+document.write("silahkan belajar dengan penuh semangat.."); */
+// alert("test");
+// var namapengunjung = prompt('silahkan masukkan nama anda', "nama saya ilham");
+// document.write('hai ' + namapengunjung + ', selamat datang di dojobox');
+// document.write('halo dojobers');
 
+//perulangan while
+var ulang=0;
+while(ulang<=10){
+    document.write('halo dojobers' + '<br>');
+    ulang+=1;
+}
