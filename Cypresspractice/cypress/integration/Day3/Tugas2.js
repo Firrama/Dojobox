91e770bd2d8701ae22cb755a80827c6d12a8cc17
Ilham <azisilham08@gmail.com>
2021-08-25 21:17:54 +0700

    Update Tugas2.js

--- Cypresspractice/cypress/integration/Day3/Tugas2.js
+++ Cypresspractice/cypress/integration/Day3/Tugas2.js
@@ -3,7 +3,9 @@ describe ('Kumpulan Testcase', function() {
         cy.visit('https://ultimateqa.com/simple-html-elements-for-automation/')
         cy.get('#et_pb_contact_name_0').type('ilham').should('have.value','ilham')
         cy.get('#et_pb_contact_email_0').type('contact.ilhamazis@gmail.com').should('have.value','contact.ilhamazis@gmail.com')
-        cy.get('.et_pb_contact_submit').click().should('exist')
+        // cy.get('.et_pb_contact_submit').click().should('exist')
+        //atau
+        cy.contains('Email Me!').click()
     }),
     it ('Nomor 2',function(){
         cy.visit('https://ultimateqa.com/simple-html-elements-for-automation/')
@@ -19,9 +21,10 @@ describe ('Kumpulan Testcase', function() {
     })
     it ('Nomor 5',function(){
         cy.visit('https://ultimateqa.com/simple-html-elements-for-automation/')
-         cy.get('.et_pb_cta_0 > .et_pb_button_wrapper > .et_pb_button').click()
-        cy.get('.entry-title').should('contain', 'Button success')
-        
+        cy.get('.et_pb_cta_0 > .et_pb_button_wrapper > .et_pb_button').click()
+        //cy.get('.entry-title').should('contain', 'Button success')
+        //atau 
+        cy.get('.entry-title').should('exist')
     })
     
     Cypress.on('uncaught:exception', (err, runnable)=>{
