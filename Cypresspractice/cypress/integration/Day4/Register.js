3f7df37c8102a158b8f11d6653aeefd52c42e958
Ilham <azisilham08@gmail.com>
2021-08-26 21:06:42 +0700

    Update Register.js

--- Cypresspractice/cypress/integration/Day4/Register.js
+++ Cypresspractice/cypress/integration/Day4/Register.js
@@ -12,6 +12,6 @@ describe('Hari Ke 4 ', function(){
             cy.wait(200)
             cy.get('#sign-username').type("hahaha").should('have.value','hahaha')
             cy.get('#sign-password').type('indonesia12').should('have.value','indonesia12')
-            cy.get('#signInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').click().should('be.selected')
+            cy.get('#signInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').click().should('exist')
         })
 })
